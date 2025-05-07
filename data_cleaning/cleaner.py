7d981652a14da51f5191c1f3511ada21648cbd8d
pedramjlo <pedram_sif@tuta.io>
2025-05-07 23:14:08 +0330

    added checkstring type

--- data_cleaning/cleaner.py
+++ data_cleaning/cleaner.py
@@ -1,7 +1,9 @@
-from uae_info.uaeInfo import UAEInfo
 import pandas as pd
 import numpy as np
+import re
 import logging
+from geopy.geocoders import Nominatim
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -9,8 +11,9 @@ logging.basicConfig(
 )
 
 class DataCleaner:
-    def __init__(self, raw_data_path):
+    def __init__(self, raw_data_path, uae_cities):
         self.raw_data_path = raw_data_path
+        self.uae_cities = uae_cities
         self.df = None
 
     def read_raw_data(self):
@@ -34,7 +37,7 @@ class DataCleaner:
         if "Address" in self.df.columns:
             if column_name not in self.df.columns:
                 try:
-                    self.df[column_name] = self.df["Address"].str.extract([city for city in UAEInfo.uae_cities()])
+                    self.df[column_name] = self.df["Address"].str.extract([city for city in self.uae_cities])
                     logging.info(f"The new column {column_name} was created.")
                 except Exception as e:
                     logging.error(e)
@@ -42,3 +45,38 @@ class DataCleaner:
                 logging.error(f"The column {column_name} already exists!")
         else:
             logging.error(f"The column {column_name} does not exist.")
+
+
+    """
+    After extracting the city name from the address column and adding it to the new City column,
+    it's removed from Address 
+    """
+    def remove_city_from_address(self):
+        if "Address" in self.df.columns:
+            for city in self.uae_cities:
+                removing_pattern = fr",?\s*{city}\s*" 
+                self.df["Address"] = self.df["Address"].str.replace(removing_pattern, '', regex=True)
+            logging.info("City names removed from Address column")
+        else:
+            logging.error("The column 'Address' does not exist.")
+
+
+    def convert_string_values(self):
+        string_columns = ['Address', 'City', 'Type', 'Rent_per_sqft', 'Frequency', 'Furnishing', 'Purpose', 'Location']
+        try:
+            self.df[string_columns] = self.df[string_columns].astype(str)
+            logging.info("Successfully converted specified columns to string type")
+        except Exception as e:
+            logging.error(f"Failed to convert some columns into strings: {e}")
+        return self.df
+
+
+    def convert_to_integer(self):
+        try:
+            numeric_cols = ['Rent', 'Beds',	'Baths', ]
+            self.df[numeric_cols] = self.df[numeric_cols].apply(pd.to_numeric, errors='coerce').astype('Int64')
+            logging.info("Successfully converted specified columns to integer type")
+        except Exception as e:
+            logging.error(f"Failed to convert some columns into integers: {e}")
+        return self.df
+
--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from data_cleaning.cleaner import DataCleaner # data_cleaning is treated as a python package
+from uae_info.uaeInfo import UAEInfo 
 
 
 
 if __name__ == "__main__":
     raw_dataset = "/Users/pedramjalali/documents/data_analysis/uae_properties/raw_data/uae_properties.csv"
 
-    cleaner = DataCleaner(raw_data_path=raw_dataset)
+    cleaner = DataCleaner(raw_data_path=raw_dataset, uae_cities=UAEInfo.uae_cities())
     data = cleaner.read_raw_data()
 
 
 
 
-    # cleaner.create_city_column()
+    #cleaner.create_city_column() # creates the city column and add the extracted city from the address column
+    #cleaner.remove_city_from_address() # removes the city name from the address column 
+    #cleaner.find_missing_latitude()
+    
+    #print(data.describe)
+    #print(data.columns)
 
-    # print(data["City"].unique())
 
-    print(data.describe)
+    cleaner.convert_string_values()
+    
+    # print(data.isna().sum())
+
+
+    """
+    - both latitude and longitude columns contain 719 out of 73742 against None for every other column
+    - 
+    """
+    # print(data.shape[0])
