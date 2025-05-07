7fce8bf6f8cc1dcef7ddf506933192ec7f35acd1
pedramjlo <pedram_sif@tuta.io>
2025-05-07 17:05:33 +0330

    added city column + relative data

--- data_cleaning/cleaner.py
+++ data_cleaning/cleaner.py
@@ -1,24 +1,23 @@
+from uae_info.uaeInfo import UAEInfo
 import pandas as pd
+import numpy as np
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,  # Set the minimum log level
-    format='%(asctime)s - %(levelname)s - %(message)s',  # Log message format
-    # filename='app.log'  # Log to a file (optional)
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
 )
 
-
-
-
 class DataCleaner:
-    def __init__(self, raw_data):
-        self.raw_data = raw_data
-        
+    def __init__(self, raw_data_path):
+        self.raw_data_path = raw_data_path
+        self.df = None
 
     def read_raw_data(self):
         try:
-            logging.info("read the dataset successfully")
-            return pd.read_csv(self.raw_data, encoding="ISO-8859-1", engine='python')
+            self.df = pd.read_csv(self.raw_data_path, encoding="ISO-8859-1", engine='python')
+            logging.info("Read the dataset successfully.")
+            return self.df
         except FileNotFoundError:
             logging.error("Error: The file was not found.")
         except pd.errors.EmptyDataError:
@@ -26,8 +25,20 @@ class DataCleaner:
         except pd.errors.ParserError:
             logging.error("Error: The file could not be parsed.")
 
-
-
-
-
-
+    
+    """
+    City names from the Address column are extracted and added to a new column City
+    """
+    def create_city_column(self):
+        column_name = "City"
+        if "Address" in self.df.columns:
+            if column_name not in self.df.columns:
+                try:
+                    self.df[column_name] = self.df["Address"].str.extract([city for city in UAEInfo.uae_cities()])
+                    logging.info(f"The new column {column_name} was created.")
+                except Exception as e:
+                    logging.error(e)
+            else:
+                logging.error(f"The column {column_name} already exists!")
+        else:
+            logging.error(f"The column {column_name} does not exist.")
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from data_cleaning.cleaner import DataCleaner # data_cleaning is treated as a py
 if __name__ == "__main__":
     raw_dataset = "/Users/pedramjalali/documents/data_analysis/uae_properties/raw_data/uae_properties.csv"
 
-    cleaner = DataCleaner(raw_data=raw_dataset)
-    read = cleaner.read_raw_data()
-
-    print(read.describe)
+    cleaner = DataCleaner(raw_data_path=raw_dataset)
+    data = cleaner.read_raw_data()
 
 
 
-    
+
+    # cleaner.create_city_column()
+
+    # print(data["City"].unique())
+
+    print(data.describe)
