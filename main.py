903ae4f46f811f76cc834f843bce82fe249540cf
pedramjlo <pedram_sif@tuta.io>
2025-06-14 13:25:44 +0330

    added pipeline class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from data_cleaning.cleaner import DataCleaner # data_cleaning is treated as a python package
 from uae_info.uaeInfo import UAEInfo 
 from data_saving.dataSaver import DataSaver
@@ -5,65 +6,45 @@ from database_utils.databaseConfig import Database
 
 
 
+class Pipeline:
+    def __init__(self, raw_data: pd.DataFrame):
+        self.raw_data = raw_data
+        self.cleaned_data = None
+
+    def data_cleaner(self):
+        cleaner = DataCleaner(raw_data=self.raw_data)
+        self.cleaned_data = cleaner.clean_all()
+
+
+    def data_saver(self):
+        saver = DataSaver()
+        saver.save_clean_data(df=self.cleaned_data)
+
+
 
 
 if __name__ == "__main__":
     raw_dataset = "/Users/pedramjalali/documents/data_analysis/uae_properties/dataset/raw_data/uae_properties.csv"
 
-    cleaner = DataCleaner(raw_data_path=raw_dataset, uae_cities=UAEInfo.uae_cities())
-    data = cleaner.read_raw_data()
+    pl = Pipeline(raw_data=raw_dataset)
+    pl.data_cleaner()
+    pl.data_saver()
 
 
-
-    # EXTRACT CITY NAME FROM ADDRESS COLUMN AND ADD IT TO THE NEW COLUMN CITY
-    
-    
-    #print(data.describe)
-    #print(data.columns)
-
-
-
-
-    # DATA CLEANING
-    cleaner.create_city_column() 
-    cleaner.remove_city_from_address() 
-    cleaner.numeric_columns()
-    cleaner.object_columns()
-    cleaner.date_columns()
-    cleaner.convert_to_date_type()
-    cleaner.impute_missing_values()
-    cleaner.drop_missing_rent_values()
-    cleaner.drop_duplicate_rows()
-    cleaner.convert_columns_to_lowercase()
 
 
     
     
 
 
-    # OUTLIER DETECTION
-    """
-
-    - Every numeric value column contains high-end and ignorable outliers meaning the distribution is 
-    positively skewed. The majority of the data is clustered toward lower values, 
-    with a few extremely high values pulling the upper end.
-
-    - Outliers are not an issue in any case.
-
-    """
-
-    #for column in data.columns:
-        #cleaner.outlier_detection(column)
-
-
-    # SAVING THE NEW CLEANED DATASET TO 'dataset/cleaned_data/'
-    saver = DataSaver()
-    saver.save_clean_data(cleaner.df)
 
 
     # LOADING THE SAVED CLEANED DATA TO POSTGRESQL DB
 
+    """
     load_to_db = Database()
     load_to_db.create_database()
     load_to_db.create_engine()
     load_to_db.load_to_db()
+    """
+    
